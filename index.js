7e11fb4213d24cdab98732cd5f2a5bd783a9412d
phohlova <hohlova.pv@edu.spbstu.ru>
2026-04-18 00:38:22 +0300

    logger

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const config = require('./config');
+const log = require('./logger');
 
-console.log(`Запуск config`, config);
+console.log(`Запуск: ${config.appName} v${config.settings.version}`);
+
+log('Сервер успешно инициализирован');
+log('Ожидание подключений на порту ' + config.settings.port);
